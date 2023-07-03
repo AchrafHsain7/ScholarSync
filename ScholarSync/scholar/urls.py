9a39805b12557a6ff849e9a32937a88d924f161c
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-07-03 20:49:10 +0100

    Home page and profile page set up

--- ScholarSync/scholar/urls.py
+++ ScholarSync/scholar/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('home/<int:id>', views.home_page, name='home'),
+    path('profile/<int:id>', views.profile_page, name='profile'),
 ]
--- ScholarSync/scholar/views.py
+++ ScholarSync/scholar/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Django!")
+    return render(request, 'scholar/index.html')
 
+def home_page(request, id):
+    return render(request, 'scholar/home.html', {
+        "id": id
+    })
+
+def profile_page(request, id):
+    return render(request, 'scholar/profile.html', {
+        "id": id
+    })
